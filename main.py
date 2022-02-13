e1453c87318a786f91328c55bad478182d78fb3e
NekoSarada1101 <ryoyukikazu11@gmail.com>
2022-02-13 23:23:18 +0900

    key指定修正

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from settings import SLACK_INCOMING_WEBHOOK_URL
 def cloud_build_notifier(event, context):
     build_message = base64.b64decode(event['data']).decode('utf-8')
     build = json.loads(build_message)
+    print(build)
 
     color = {
         "SUCCESS": "#28a745",
@@ -17,26 +18,26 @@ def cloud_build_notifier(event, context):
     data = {
         "attachments": [
             {
-                "color": color[build.status],
+                "color": color[build['status']],
                 "blocks": [
                     {
                         "type": "section",
                         "fields": [
                             {
                                 "type": "mrkdwn",
-                                "text": "*status:*\n{}".format(build.status)
+                                "text": "*status:*\n{}".format(build['status'])
                             },
                             {
                                 "type": "mrkdwn",
-                                "text": "*id:*\n{}".format(build.id)
+                                "text": "*id:*\n{}".format(build['id'])
                             },
                             {
                                 "type": "mrkdwn",
-                                "text": "*start:*\n{}".format(build.startTime)
+                                "text": "*start:*\n{}".format(build['startTime'])
                             },
                             {
                                 "type": "mrkdwn",
-                                "text": "*finish:*\n{}".format(build.finishTime)
+                                "text": "*finish:*\n{}".format(build['finishTime'])
                             },
                         ]
                     },
@@ -46,11 +47,11 @@ def cloud_build_notifier(event, context):
                                 {
                                     "type": "button",
                                     "text": {
-                                            "type": "plain_text",
+                                        "type": "plain_text",
                                         "text": "View log"
                                     },
                                     "style": "primary",
-                                    "url": build.logUrl,
+                                    "url": build['logUrl'],
                                 },
                         ]
                     }
